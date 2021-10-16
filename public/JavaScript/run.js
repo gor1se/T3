4ad3af9aefe822684787647a5f59a73eb9e0b3d3
gor1se <andreas.schmidt.lth@t-online.de>
2021-10-16 19:15:55 +0200

    Keydetection ausgebaut, Funktion noch nicht gegeben.Testfeld implementiert

--- public/JavaScript/run.js
+++ public/JavaScript/run.js
@@ -1,25 +1,50 @@
 let text = document.getElementById("input-text");
 let started = false;
 
+let text_array = [];
+let part_1 = document.getElementById("part1-array");
+let part_2 = document.getElementById("part2-array");
+let part_3 = document.getElementById("part3-array");
+let testdiv = document.getElementById("testdiv");
+part_1.innerText = "";
+part_2.innerText = "";
+part_3.innerText = "";
+let part1_array = [];
+let part2_array = [];
+let part3_array = [];
+let input_key = 0;
+
 document.body.addEventListener("keydown", function(evt) {
+    testdiv.innerText = evt.key;
     // Starte erst sobald space gedrückt wurde
-    if (evt.keyCode == 32 && started == false) {
+    if (true) { //evt.keyCode == 32 && started == false
         started = true;
-        alert("Los gehts!");
-        let text_array = text.innerText.split("");
-        let part_1 = document.getElementById("part1-array");
-        let part_2 = document.getElementById("part2-array");
-        let part1_array = ["a", "b", "c"];
-        let part2_array = ["c", "d"];
-        part2_array = text;
-        text_array.forEach(function(item) {
-            document.getElementById("original-text").style.display = "none";
-            part_1.innerText = part1_array.join("");
-            alert("HI")
-            part_2.innerText = part2_array.join("");
-            alert("HI2")
-        });
+        text_array = text.innerText.split("");
+        document.getElementById("original-text").style.display = "none";
+        part1_array = [];
+        part2_array = text_array[0];
+        part3_array = text_array;
+        // Lösche erstes Element von part3
+        part3_array.shift();
     }
+    text_array.forEach(function(item) {
+        input_key = evt.keyCode;
+        expected_key = part2_array;
+
+        // Hier wird später die Eingabe überprüft, hier wird einfach true zurückgegeben
+        if (input_key == expected_key.charCodeAt()) {
+            part1_array.push(part2_array[0]);
+            part2_array = part3_array[0];
+            part3_array.shift();
+            part_1.innerText = part1_array.join("");
+            part_2.innerText = part2_array; // Da nur ein einziges Element kein join nötig
+            part_3.innerText = part3_array.join("");
+        } else {
+            // Code wenn die eingabe falsch war
+        }
+
+    });
+
     // Erster Buchstabe dunkel Grau
 
 
